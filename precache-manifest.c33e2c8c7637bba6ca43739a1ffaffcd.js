041a8696cee242f2d455cdad9df2243c033b7527
AHannelais <antoine.hannelais@gmail.com>
2019-08-28 10:38:05 +0200

    Updates

---
+++ precache-manifest.c33e2c8c7637bba6ca43739a1ffaffcd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a10faeacdd905c5e7f7c338ef1f61a",
+    "url": "/Daily-Science-Article/index.html"
+  },
+  {
+    "revision": "9f97e70c87f419b611ad",
+    "url": "/Daily-Science-Article/static/css/2.f9f4654f.chunk.css"
+  },
+  {
+    "revision": "d056ea4a611372812215",
+    "url": "/Daily-Science-Article/static/css/main.8598fcff.chunk.css"
+  },
+  {
+    "revision": "9f97e70c87f419b611ad",
+    "url": "/Daily-Science-Article/static/js/2.5f3375c3.chunk.js"
+  },
+  {
+    "revision": "d056ea4a611372812215",
+    "url": "/Daily-Science-Article/static/js/main.b5bed145.chunk.js"
+  },
+  {
+    "revision": "87529b53b1de42661635",
+    "url": "/Daily-Science-Article/static/js/runtime~main.48225bcc.js"
+  },
+  {
+    "revision": "d1cb154f70adfd812d98d4f643cd8045",
+    "url": "/Daily-Science-Article/static/media/Bevan.d1cb154f.ttf"
+  },
+  {
+    "revision": "620460d96c0ec8988430f909e75493e0",
+    "url": "/Daily-Science-Article/static/media/Philosopher-Bold-webfont.620460d9.woff"
+  },
+  {
+    "revision": "961fa76b7eca2727474259364efd4e6c",
+    "url": "/Daily-Science-Article/static/media/Philosopher-Bold-webfont.961fa76b.ttf"
+  },
+  {
+    "revision": "f66e60f04fc5cad9cfa762d2c46a7ab0",
+    "url": "/Daily-Science-Article/static/media/Philosopher-Bold-webfont.f66e60f0.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Daily-Science-Article/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Daily-Science-Article/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Daily-Science-Article/static/media/revicons.2feb69cc.eot"
+  }
+]);
